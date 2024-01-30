0abb83858c41807891a19562479e9b325a595984
balpreetcode <90306880+balpreetcode@users.noreply.github.com>
2024-01-31 00:11:35 +0530

    test123

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 import cors from "cors";
 import dotenv from "dotenv";
+import { initializeApp } from "firebase/app";
+import { getFirestore, doc, setDoc, getDoc } from "firebase/firestore";
 
 dotenv.config();
 app.use(cors());
@@ -10,8 +12,43 @@ app.use(cors());
 app.listen(PORT, () => {
   console.log(`Backend is running on port ${PORT}`);
 });
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBpJ259fUt9afgnFOEabtjoKbEwbFm_gZ8",
+  authDomain: "vrchat-2f58c.firebaseapp.com",
+  projectId: "vrchat-2f58c",
+  storageBucket: "vrchat-2f58c.appspot.com",
+  messagingSenderId: "315680730497",
+  appId: "1:315680730497:web:acd9cd885946d0bc33cc80",
+  measurementId: "G-JFJVFZS1H3"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+
 app.get("/", (req, res) => {
   res.send("Welcome to the API!");
+});
+
+app.post('/add', async (req, res) => {
+  const docRef = doc(db, 'users', 'user1');
+  await setDoc(docRef, {
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    age: 30
+  });
+  res.send('Data added to Firebase');
+});
+
+// Route to get data from Firebase
+app.get('/get', async (req, res) => {
+  const docRef = doc(db, 'users', 'user1');
+  const docSnap = await getDoc(docRef);
+  if (!docSnap.exists()) {
+    res.status(404).send('No such document!');
+  } else {
+    res.send(docSnap.data());
+  }
 });
 
 // Route for 'long'
